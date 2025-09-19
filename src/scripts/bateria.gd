bea3a1217708c61f949dcc50a13a5c3b810d8349
Argchacal <ayala972@gmail.com>
2025-09-19 14:24:36 -0300

    all

---
+++ src/scripts/bateria.gd
@@ -0,0 +1,8 @@
+extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animated_sprite_2d.play("idle")
+	
